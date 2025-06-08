fca554e52684addb7c94b0d42bccbcf1844b6c77
Raj Chandravadiya <raj.chandravadiya21@gmail.com>
2025-06-08 23:10:28 +0530

    feat(common): add user model in sequalize

--- common/src/sequalize/db/context.ts
+++ common/src/sequalize/db/context.ts
@@ -1,16 +1,19 @@
 import { Sequelize } from 'sequelize';
 import { LookupDetailsDefineModel, LookupDetailsModelStatic } from '../models/lookup-details';
 import { LookupsDefineModel, LookupsModelStatic } from '../models/lookups';
+import { UserDefineModel, UserModelStatic } from '../models/user';
 import { sequelize } from './config';
 
 export interface DbContext {
   sequelize: Sequelize;
   lookups: LookupsModelStatic;
   lookupDetails: LookupDetailsModelStatic;
+  user: UserModelStatic;
 }
 
 export const db: DbContext = {
   sequelize: sequelize,
   lookups: LookupsDefineModel,
-  lookupDetails: LookupDetailsDefineModel
+  lookupDetails: LookupDetailsDefineModel,
+  user: UserDefineModel
 };
