c1a9e6a57146827d5a8ef4d8f6d29c903a39313c
Raj Chandravadiya <raj.chandravadiya21@gmail.com>
2025-06-11 22:14:14 +0530

    fix(common): export models into index.ts

--- common/index.ts
+++ common/index.ts
@@ -1,3 +1,8 @@
+import {
+  BuyerAddressesCreateModel,
+  BuyerAddressesUpdateModel,
+  BuyerAddressesModel
+} from './src/sequalize/models/buyer-addresses';
 export { db, DbContext } from './src/sequalize/db/context';
 export { sequelize } from './src/sequalize/db/config';
 export {
@@ -30,3 +35,28 @@ export {
   ProductVarientUpdateModel,
   ProductVarientModel
 } from './src/sequalize/models/product-varient';
+export {
+  BuyerAddressesCreateModel,
+  BuyerAddressesUpdateModel,
+  BuyerAddressesModel
+} from './src/sequalize/models/buyer-addresses';
+export {
+  SellerCreateModel,
+  SellerUpdateModel,
+  SellerModel
+} from './src/sequalize/models/seller';
+export {
+  SellerAddressesCreateModel,
+  SellerAddressesUpdateModel,
+  SellerAddressesModel
+} from './src/sequalize/models/seller-addresses';
+export {
+  SellerDocumentCreateModel,
+  SellerDocumentUpdateModel,
+  SellerDocumentModel
+} from './src/sequalize/models/seller-document';
+export {
+  SellerReviewCreateModel,
+  SellerReviewUpdateModel,
+  SellerReviewModel
+} from './src/sequalize/models/seller-review';
